418ac264eaea5361a47b8da5c6a6bba19e8f27d8
Jan Szumiec <jan.szumiec@gmail.com>
2023-09-06 21:40:45 +0200

    Stub for tests.

---
+++ tests/alu.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, Deserialize)]
+struct TestEntry {
+    x: String,
+    y: String,
+    flags: String,
+    result: TestResult
+}
+
+#[derive(Debug, Deserialize)]
+struct TestResult {
+    value: String,
+    flags: String
+}
+
+#[test]
+fn test_add() {
+    let mut file = File::open("sm83-test-data/alu_tests/v1/add.json").unwrap();
+    let json: Vec<TestEntry> = serde_json::from_reader(file).expect("bad json");
+}
